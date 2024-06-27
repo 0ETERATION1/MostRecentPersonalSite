b67ee1da82c3ef23088f9ea14798b3c1ebf136da
Alex Terskin <terskinalex@gmail.com>
2024-06-27 15:19:54 -0400

    finish preloader

--- src/App/UI/Preloader.js
+++ src/App/UI/Preloader.js
@@ -13,15 +13,38 @@ export default class Preloader {
         this.assetStore.subscribe((state) => {
             console.log(state.loadedAssets);
 
-            
+
             // loading screen
             this.numberOfLoadedAssets = Object.keys(state.loadedAssets).length;
             this.numberOfAssetsToLoad = state.assetsToLoad.length
             this.progress = this.numberOfLoadedAssets / this.numberOfAssetsToLoad;
             this.progress = Math.trunc(this.progress * 100);
-
             document.getElementById('progressPercentage').innerHTML = this.progress;
-            console.log(this.progress);
+
+            // adding start button if we get to 100%
+            if (this.progress === 100) {
+                this.loading.classList.add('fade');
+                window.setTimeout(() => this.ready(), 1000 )
+                
+            }
         })
+    }
+
+    ready() {
+        this.loading.remove();
+        this.startButton.style.display = 'inline';
+        this.startButton.classList.add('fadeIn');
+
+        this.startButton.addEventListener('click', () => {
+            console.log('clicked');
+            this.overlay.classList.add('fade');
+            this.startButton.classList.add('fadeOut');
+
+            window.setTimeout(() => {
+                this.overlay.remove()
+                this.startButton.remove()
+            }, 2000)
+
+        }, {once: true})
     }
 }
