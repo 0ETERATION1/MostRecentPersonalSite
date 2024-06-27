867b740e5ab75708a9bc1dc1478b2731a06538f6
Alex Terskin <terskinalex@gmail.com>
2024-06-27 15:08:26 -0400

    update loading screen

--- src/App/UI/Preloader.js
+++ src/App/UI/Preloader.js
@@ -3,12 +3,24 @@ import assetStore from "../Utils/AssetStore";
 export default class Preloader {
     constructor() {
         this.assetStore = assetStore;
+
+        // giving access to dom
+        this.overlay = document.querySelector('.overlay');
+        this.loading = document.querySelector('.loading');
+        this.startButton = document.querySelector('.start');
+
+
         this.assetStore.subscribe((state) => {
             console.log(state.loadedAssets);
+
+            
             // loading screen
             this.numberOfLoadedAssets = Object.keys(state.loadedAssets).length;
             this.numberOfAssetsToLoad = state.assetsToLoad.length
             this.progress = this.numberOfLoadedAssets / this.numberOfAssetsToLoad;
+            this.progress = Math.trunc(this.progress * 100);
+
+            document.getElementById('progressPercentage').innerHTML = this.progress;
             console.log(this.progress);
         })
     }
