f8a41424f66b1708de221d80440c92f6063f8b6b
Alex Terskin <terskinalex@gmail.com>
2024-07-01 17:34:16 -0400

    update dependencies

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,12 +1,18 @@
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    plugins: [
-        wasm(),
-        topLevelAwait()
-      ]
-}
+
+export default defineConfig({
+ root: 'src/',
+ publicDir: '../static/',
+ base: './',
+ build: {
+   outDir: '../dist', // Output directory relative to the project root
+   emptyOutDir: true  // Ensures the directory is emptied before building
+ },
+ plugins: [
+   wasm(),
+   topLevelAwait()
+ ]
+});
