5e7a5e962b9096640f41007077840910cda46e0a
Alex Terskin <terskinalex@gmail.com>
2024-07-01 16:32:52 -0400

    new environment

--- my-app/src/App/World/Environment.js
+++ my-app/src/App/World/Environment.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
-import assetStore from "../Utils/AssetStore.js";
-
 import App from "../App.js";
+import assetStore from "../Utils/AssetStore.js";
 
 export default class Environment {
   constructor() {
@@ -9,13 +8,12 @@ export default class Environment {
     this.scene = this.app.scene;
     this.physics = this.app.world.physics;
     this.pane = this.app.gui.pane;
-    this.assetStore = assetStore.getState();
-    this.environment = this.assetStore.loadedAssets.environment;
 
-  
+    this.assetStore = assetStore.getState()
+    this.environment = this.assetStore.loadedAssets.environment
+
     this.loadEnvironment();
     this.addLights();
-    this.addGround();
   }
 
   loadEnvironment() {
@@ -23,7 +21,60 @@ export default class Environment {
     const environmentScene = this.environment.scene;
     this.scene.add(environmentScene);
 
-    
+    environmentScene.position.set(-4.8, 0, -7.4)
+    environmentScene.rotation.set(0, -.60, 0)
+    environmentScene.scale.setScalar(1.3)
+
+    const physicalObjects = ['trees',
+      'terrain',
+      'rocks',
+      'stairs',
+      'gates',
+      'floor',
+      'bushes'
+    ]
+
+    const shadowCasters = ['trees',
+      'terrain',
+      'rocks',
+      'stairs',
+      'gates',
+      'bushes'
+    ]
+
+    const shadowReceivers = ['floor',
+    'terrain'
+  ]
+
+    for (const child of environmentScene.children) {
+      const isPhysicalObject = physicalObjects.some((keyword) => child.name.includes(keyword))
+      if (isPhysicalObject) {
+        child.traverse((obj) => {
+          if (obj.isMesh) {
+            this.physics.add(obj, "fixed", "cuboid")
+          }
+        })
+      }
+
+      const isShadowCaster = shadowCasters.some((keyword) => child.name.includes(keyword))
+      if (isShadowCaster) {
+        child.traverse((obj) => {
+          if (obj.isMesh) {
+            obj.castShadow = true
+          }
+        })
+      }
+
+      const isShadowReceiver = shadowReceivers.some((keyword) => child.name.includes(keyword))
+      if (isShadowReceiver) {
+        child.traverse((obj) => {
+          if (obj.isMesh) {
+            obj.receiveShadow = true
+          }
+        })
+      }
+    }
+
   }
 
 
@@ -34,16 +85,13 @@ export default class Environment {
     this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
     this.directionalLight.position.set(1, 1, 1);
     this.directionalLight.castShadow = true;
-    this.scene.add(this.directionalLight);
+    this.directionalLight.shadow.camera.top = 30
+    this.directionalLight.shadow.camera.right = 30
+    this.directionalLight.shadow.camera.left = -30
+    this.directionalLight.shadow.camera.bottom = -30
+    this.directionalLight.shadow.bias = -0.002
+    this.directionalLight.shadow.normalBias = 0.072
+     this.scene.add(this.directionalLight);
   }
 
-  addGround() {
-    const groundGeometry = new THREE.BoxGeometry(100, 1, 100);
-    const groundMaterial = new THREE.MeshStandardMaterial({
-      color: "turquoise",
-    });
-    this.groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-    this.scene.add(this.groundMesh);
-    this.physics.add(this.groundMesh, "fixed", "cuboid");
-  }
 }
