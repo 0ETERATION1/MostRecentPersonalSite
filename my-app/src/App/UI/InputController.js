af6d8db2fc668d230ea41c0e1f63544e3635720f
Alex Terskin <terskinalex@gmail.com>
2024-07-03 11:45:40 -0400

    nipple.js

--- my-app/src/App/UI/InputController.js
+++ my-app/src/App/UI/InputController.js
@@ -1,101 +1,91 @@
 import { inputStore } from "../Utils/Store";
+import nipplejs from 'nipplejs';
 
 export default class InputController {
-  constructor() {
-    this.startListening();
-    this.inputStore = inputStore;
-    this.keyPressed = {};
-    this.touchStartX = 0;
-    this.touchStartY = 0;
-  }
+    constructor() {
+        this.startListening();
+        this.inputStore = inputStore;
+        this.keyPressed = {};
+        this.touchStartX = 0;
+        this.touchStartY = 0;
 
-  startListening() {
-    window.addEventListener("keydown", (event) => this.onKeyDown(event));
-    window.addEventListener("keyup", (event) => this.onKeyUp(event));
-    window.addEventListener("touchstart", (event) => this.onTouchStart(event));
-    window.addEventListener("touchmove", (event) => this.onTouchMove(event));
-  }
-
-  onKeyDown(event) {
-    if (!this.keyPressed[event.code]) {
-      switch (event.code) {
-        case "KeyW":
-        case "ArrowUp":
-          console.log("forward");
-          inputStore.setState({ forward: true });
-          break;
-        case "KeyA":
-        case "ArrowLeft":
-          inputStore.setState({ left: true });
-          break;
-        case "KeyS":
-        case "ArrowDown":
-          inputStore.setState({ backward: true });
-          break;
-        case "KeyD":
-        case "ArrowRight":
-          inputStore.setState({ right: true });
-          break;
-      }
-      this.keyPressed[event.code] = true;
-    }
-  }
-
-  onKeyUp(event) {
-    switch (event.code) {
-      case "KeyW":
-      case "ArrowUp":
-        inputStore.setState({ forward: false });
-        break;
-      case "KeyA":
-      case "ArrowLeft":
-        inputStore.setState({ left: false });
-        break;
-      case "KeyS":
-      case "ArrowDown":
-        inputStore.setState({ backward: false });
-        break;
-      case "KeyD":
-      case "ArrowRight":
-        inputStore.setState({ right: false });
-        break;
-    }
-    this.keyPressed[event.code] = false;
-  }
-
-  onTouchStart(event) {
-    this.touchStartX = event.touches[0].clientX;
-    this.touchStartY = event.touches[0].clientY;
-  }
-
-  onTouchMove(event) {
-    if (event.touches.length > 1) return; // Ignore multi-touch
-
-    let touchEndX = event.touches[0].clientX;
-    let touchEndY = event.touches[0].clientY;
-
-    let diffX = touchEndX - this.touchStartX;
-    let diffY = touchEndY - this.touchStartY;
-
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-      if (diffX > 0) {
-        inputStore.setState({ right: true });
-        inputStore.setState({ left: false });
-      } else {
-        inputStore.setState({ left: true });
-        inputStore.setState({ right: false });
-      }
-    } else {
-      if (diffY > 0) {
-        inputStore.setState({ backward: true });
-        inputStore.setState({ forward: false });
-      } else {
-        inputStore.setState({ forward: true });
-        inputStore.setState({ backward: false });
-      }
+        this.initJoystick();
     }
 
-    this.touchStartX = touchEndX;
-    this.touchStartY = touchEndY;
-  }
+    startListening() {
+        window.addEventListener("keydown", (event) => this.onKeyDown(event));
+        window.addEventListener("keyup", (event) => this.onKeyUp(event));
+    }
+
+    onKeyDown(event) {
+        if (!this.keyPressed[event.code]) {
+            switch (event.code) {
+                case "KeyW":
+                case "ArrowUp":
+                    inputStore.setState({ forward: true });
+                    break;
+                case "KeyA":
+                case "ArrowLeft":
+                    inputStore.setState({ left: true });
+                    break;
+                case "KeyS":
+                case "ArrowDown":
+                    inputStore.setState({ backward: true });
+                    break;
+                case "KeyD":
+                case "ArrowRight":
+                    inputStore.setState({ right: true });
+                    break;
+            }
+            this.keyPressed[event.code] = true;
+        }
+    }
+
+    onKeyUp(event) {
+        switch (event.code) {
+            case "KeyW":
+            case "ArrowUp":
+                inputStore.setState({ forward: false });
+                break;
+            case "KeyA":
+            case "ArrowLeft":
+                inputStore.setState({ left: false });
+                break;
+            case "KeyS":
+            case "ArrowDown":
+                inputStore.setState({ backward: false });
+                break;
+            case "KeyD":
+            case "ArrowRight":
+                inputStore.setState({ right: false });
+                break;
+        }
+        this.keyPressed[event.code] = false;
+    }
+
+    initJoystick() {
+        const joystick = nipplejs.create({
+            zone: document.body,
+            mode: 'static',
+            position: { left: '50%', bottom: '50px' },
+            color: 'blue'
+        });
+
+        joystick.on('move', (evt, data) => {
+            const angle = data.angle.degree;
+            if (angle >= 45 && angle < 135) {
+                inputStore.setState({ forward: true, backward: false });
+            } else if (angle >= 135 && angle < 225) {
+                inputStore.setState({ left: true, right: false });
+            } else if (angle >= 225 && angle < 315) {
+                inputStore.setState({ backward: true, forward: false });
+            } else {
+                inputStore.setState({ right: true, left: false });
+            }
+        });
+
+        joystick.on('end', () => {
+            inputStore.setState({ forward: false, backward: false, left: false, right: false });
+        });
+    }
 }
