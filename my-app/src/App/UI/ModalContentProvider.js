b27ddf504e1cfac5813aefad786caac7d83ca3e7
Alex Terskin <terskinalex@gmail.com>
2024-07-09 17:13:39 -0400

    update portal1

--- my-app/src/App/UI/ModalContentProvider.js
+++ my-app/src/App/UI/ModalContentProvider.js
@@ -4,9 +4,21 @@ export default class ModalContentProvider {
         aboutMe: {
           title: 'About me',
           description: 
-          `<p>Hello, I am a software engineer with a passion for developing innovative programs that expedite the efficiency and effectiveness of organizational success. I am experienced in technology and writing code to create systems that are reliable and user-friendly.</p>
-                    <p>You can find more about my work on my <a href="https://github.com/yourgithub" target="_blank">GitHub</a> or connect with me on <a href="https://www.linkedin.com/in/yourlinkedin" target="_blank">LinkedIn</a>.</p>
-                    <p>Download my resume <a href="/resume/AlexTerskin.pdf" target="_blank">here</a>.</p>
+          `
+          <h3>I'm Alex Terskin</h3>
+          <p>Currently I am a computer science student at the University of Maryland!</p>
+          <p>
+          Thank you for taking the time to check out my portfolio. 
+          I really hope you enjoy exploring it as much as I enjoyed building it. 
+          If you have any questions or comments, feel free to connect with me on 
+          <a href="https://www.linkedin.com/in/alexanderterskin/" target="_blank">linkedin</a>
+          or contact me directly by shooting me an email @ 
+          <a href="mailto:terskinlalex@gmail.com" targer="_blank">terskinalex@gmail.com</a>
+                        
+          </p>
+
+          <h1>Looking for my resume?</h1>
+          <p>Download it here! <a href="/resume/AlexTerskin.pdf" target="_blank">here</a>.</p>
           `
         },
         projects: {
